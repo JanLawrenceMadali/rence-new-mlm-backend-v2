7452c4ee92fef6ce7d1320b96643260dacd68da9
Rence_Madali <jan.lawrence.madali@gmail.com>
2023-11-07 22:12:56 +0800

    Roles added

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -4,8 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Role extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['name'];
+
+    public function users(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
