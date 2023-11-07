4c167f389193d4d30ee5a8608e8e394695a0c0ef
Rence_Madali <jan.lawrence.madali@gmail.com>
2023-11-07 22:13:51 +0800

    mock users added

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -21,10 +21,11 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
+            'role_id' => rand(2, 3),
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
+            'password' => bcrypt('@dGmW3b2023'),
             'remember_token' => Str::random(10),
         ];
     }
